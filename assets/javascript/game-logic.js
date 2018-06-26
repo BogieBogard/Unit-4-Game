666277c8db240eed69f95f948fc95db156b2c734
BogieBogard <mbogard@utexas.edu>
2018-06-26 18:01:01 -0500

    Early stage of setting up the game's logic

--- assets/javascript/game-logic.js
+++ assets/javascript/game-logic.js
@@ -1,11 +1,17 @@
 // Generate random number that the user will try to match, with Math.random and Math.floor
-var startingRandomNumber = Math.floor(Math.random() * 101 + 19)
+var startingRandomNumber = Math.floor(Math.random() * 101 + 19);
 
+document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
 
+// Declare the user's Total Points and connect the crystals to that variable
+let userTP = 0
+
+document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
 
 // Total variable from the user's input using the crytals
 
 // Generate the four random numbers for each crystal
+var crystal1Value = Math.floor(Math.random() * 10)
 
 // Create on.click function for the crystals to add to the total variable
 $ ("#CrystalButton1").on("click", function(event){
