4e796774c5f25babd34a24b1aef689ce0ae5cdd1
BogieBogard <mbogard@utexas.edu>
2018-06-26 15:11:53 -0500

    Getting the ball rolling

---
+++ assets/javascript/game-logic.js
@@ -0,0 +1,17 @@
+// Generate random number that the user will try to match, with Math.random and Math.floor
+var startingRandomNumber = Math.floor(Math.random() * 101 + 19)
+
+
+
+// Total variable from the user's input using the crytals
+
+// Generate the four random numbers for each crystal
+
+// Create on.click function for the crystals to add to the total variable
+$ ("#CrystalButton1").on("click", function(event){
+    
+});
+// Create function for win/lose for comparing the total with the given random number
+
+
+
