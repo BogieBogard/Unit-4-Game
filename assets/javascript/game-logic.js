38e6a4df9c006bc0086872570a62bd52459b13e7
BogieBogard <mbogard@utexas.edu>
2018-06-27 19:51:02 -0500

    Improvements in the JS

--- assets/javascript/game-logic.js
+++ assets/javascript/game-logic.js
@@ -3,21 +3,50 @@ var startingRandomNumber = Math.floor(Math.random() * 101 + 19);
 
 document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
 
+// Declare variables for winning and losing and set up their display
+var winner = 0;
+
+document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
+
+var loser = 0;
+
+document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
+
 // Declare the user's Total Points and connect the crystals to that variable
-let userTP = 0
+var userTP = 0;
 
 document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
 
 // Total variable from the user's input using the crytals
 
 // Generate the four random numbers for each crystal
-var crystal1Value = Math.floor(Math.random() * 10)
+var crystal1Value = Math.floor(Math.random() * 10 + 1);
 
 // Create on.click function for the crystals to add to the total variable
-$ ("#CrystalButton1").on("click", function(event){
-    
+$("document").ready(function () {
+    $("#CrystalButton1").on("click", function (event) {
+        userTP = crystal1Value + userTP;
+        console.log(userTP);
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+    });
+    // Create function for win/lose for comparing the total with the given random number
+    var i;
+    for (winner = 0; winner < 10; winner++) {
+        if (userTP === startingRandomNumber) {
+            console.log("Have a good day!");
+            winner++;
+            console.log(winner);
+            document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
+        };
+
+        if (userTP > 10) {
+            console.log("Over ten!");
+            loser++;
+            console.log(loser);
+            document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
+        };
+
+    };
+
 });
-// Create function for win/lose for comparing the total with the given random number
-
-
 
