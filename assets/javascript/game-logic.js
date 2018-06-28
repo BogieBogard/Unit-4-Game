0f22cd86c4f1cb5f8f459d515d597af2410d72cb
BogieBogard <mbogard@utexas.edu>
2018-06-28 10:45:28 -0500

    The JS meets the requirements of the assignment.

--- assets/javascript/game-logic.js
+++ assets/javascript/game-logic.js
@@ -21,6 +21,9 @@ document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + 
 
 // Generate the four random numbers for each crystal
 var crystal1Value = Math.floor(Math.random() * 10 + 1);
+var crystal2Value = Math.floor(Math.random() * 10 + 1);
+var crystal3Value = Math.floor(Math.random() * 10 + 1);
+var crystal4Value = Math.floor(Math.random() * 10 + 1);
 
 // Create on.click function for the crystals to add to the total variable
 $("#CrystalButton1").on("click", function (event) {
@@ -33,6 +36,7 @@ $("#CrystalButton1").on("click", function (event) {
         console.log(winner);
         document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
         userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
         crystal1Value = Math.floor(Math.random() * 10 + 1);
         startingRandomNumber = Math.floor(Math.random() * 101 + 19);
         document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
@@ -43,6 +47,7 @@ $("#CrystalButton1").on("click", function (event) {
         console.log(loser);
         document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
         userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
         crystal1Value = Math.floor(Math.random() * 10 + 1);
         startingRandomNumber = Math.floor(Math.random() * 101 + 19);
         document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
@@ -50,4 +55,96 @@ $("#CrystalButton1").on("click", function (event) {
 
 });
 
+// Replicating for Crystal Button 2
+// But there's a better method than repeating the same code for the additional crystals
 
+$("#CrystalButton2").on("click", function (event) {
+    userTP = crystal2Value + userTP;
+    console.log(userTP);
+    document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+
+    if (userTP === startingRandomNumber) {
+        winner++;
+        console.log(winner);
+        document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+    if (userTP > startingRandomNumber) {
+        loser++;
+        console.log(loser);
+        document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+});
+
+// Replicating for Crystal Button 3
+
+$("#CrystalButton3").on("click", function (event) {
+    userTP = crystal3Value + userTP;
+    console.log(userTP);
+    document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+
+    if (userTP === startingRandomNumber) {
+        winner++;
+        console.log(winner);
+        document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+    if (userTP > startingRandomNumber) {
+        loser++;
+        console.log(loser);
+        document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+});
+
+// Replicating for Crystal Button 4
+
+$("#CrystalButton4").on("click", function (event) {
+    userTP = crystal4Value + userTP;
+    console.log(userTP);
+    document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+
+    if (userTP === startingRandomNumber) {
+        winner++;
+        console.log(winner);
+        document.getElementById("playerWins").innerHTML = ("Player Wins: " + winner);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+    if (userTP > startingRandomNumber) {
+        loser++;
+        console.log(loser);
+        document.getElementById("playerLosses").innerHTML = ("Player Losses: " + loser);
+        userTP = 0;
+        document.getElementById("userTotalPoints").innerHTML = ("Your Total Points: " + userTP);
+        crystal1Value = Math.floor(Math.random() * 10 + 1);
+        startingRandomNumber = Math.floor(Math.random() * 101 + 19);
+        document.getElementById("initialNumberChallenge").innerHTML = ("Meet this number exactly: " + startingRandomNumber);
+    };
+
+});
